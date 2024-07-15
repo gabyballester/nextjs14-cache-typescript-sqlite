7b611ddaba374f72228e57cc26ad1b292590e6be
gabyballester <gaby.ballester@gmail.com>
2024-07-15 14:45:48 +0200

    Add new message page  and error handling

---
+++ app/messages/new/page.tsx
@@ -0,0 +1,33 @@
+import { addMessage } from "@/services";
+import { redirect } from "next/navigation";
+
+export default function NewMessagePage() {
+  async function createMessage(formData: FormData) {
+    "use server";
+
+    const message = formData.get("message");
+
+    if (typeof message === "string" && message.length > 0) {
+      addMessage(message);
+      redirect("/messages");
+    } else {
+      console.error("Message is not valid");
+      throw new Error("Message is not valid");
+    }
+  }
+  return (
+    <>
+      <h2>New Message</h2>
+      <form action={createMessage}>
+        <p className="form-control">
+          <label htmlFor="message">Your Message</label>
+          <textarea id="message" name="message" required rows={5} />
+        </p>
+
+        <p className="form-actions">
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    </>
+  );
+}
